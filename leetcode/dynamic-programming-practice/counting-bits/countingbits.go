9c23f56ae696e4f944f01002369a1cf1b668444a
aaronc <aaron.cassidy1995@gmail.com>
2026-06-07 18:07:27 +0100

    Add implementation of counting bits

--- leetcode/dynamic-programming-practice/counting-bits/countingbits.go
+++ leetcode/dynamic-programming-practice/counting-bits/countingbits.go
@@ -5,5 +5,21 @@ package countingbits
 
 func CountBits(n int) []int {
 	var bitcounts []int
+
+	bitcounts = append(bitcounts, 0)
+	for i := 1; i <= n; i++ {
+
+		// Powers of two only contain 1 zero so can skip computation
+		if (i & (i - 1)) == 0 {
+			bitcounts = append(bitcounts, 1)
+		} else {
+			if i%2 != 0 {
+				bitcounts = append(bitcounts, bitcounts[i-1]+1)
+			} else {
+				bitcounts = append(bitcounts, bitcounts[i-1])
+			}
+		}
+	}
+
 	return bitcounts
 }

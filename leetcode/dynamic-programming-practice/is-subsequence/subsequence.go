7e6f4f113487ea25d12493de08649153df2856d8
aaronc <aaron.cassidy1995@gmail.com>
2026-06-07 19:04:05 +0100

    Add impl based on sliding-window which passes all test cases

--- leetcode/dynamic-programming-practice/is-subsequence/subsequence.go
+++ leetcode/dynamic-programming-practice/is-subsequence/subsequence.go
@@ -8,5 +8,18 @@ package issubsequence
 
 func IsSubsequence(s, t string) bool {
 	var found bool
+	var i, j int
+
+	found = true
+	for i, j = 0, 0; i != len(s)-1 && j != len(t)-1; j++ {
+		if s[i] == t[j] {
+			i++
+		}
+	}
+
+	if i != len(s)-1 {
+		found = false
+	}
+
 	return found
 }

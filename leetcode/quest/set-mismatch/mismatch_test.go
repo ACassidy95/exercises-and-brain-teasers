7f3456a63b7fe2abcf232acdcedd57b6c68020a7
aaronc <aaron.cassidy1995@gmail.com>
2026-06-10 11:46:26 +0100

    Add test cases for set mismatch

--- leetcode/quest/set-mismatch/mismatch_test.go
+++ leetcode/quest/set-mismatch/mismatch_test.go
@@ -10,7 +10,24 @@ func TestSetMismatch(t *testing.T) {
 	testCases := map[string]struct {
 		nums     []int
 		expected []int
-	}{}
+	}{
+		"[1, 2, 2, 4]": {
+			nums:     []int{1, 2, 2, 4},
+			expected: []int{2, 3},
+		},
+		"[1, 1]": {
+			nums:     []int{1, 1},
+			expected: []int{1, 2},
+		},
+		"[2, 2]": {
+			nums:     []int{2, 2},
+			expected: []int{2, 2},
+		},
+		"[3, 2, 2]": {
+			nums:     []int{3, 2, 2},
+			expected: []int{2, 1},
+		},
+	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {

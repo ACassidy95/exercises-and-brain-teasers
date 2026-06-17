416007ff31b2b3dfff9c55ddf2276a63328b6980
aaronc <aaron.cassidy1995@gmail.com>
2026-06-17 20:26:24 +0100

    Progress, tests closer to paassing

--- leetcode/dynamic-programming-practice/jump/jump.go
+++ leetcode/dynamic-programming-practice/jump/jump.go
@@ -1,5 +1,34 @@
 package jump
 
+import "math"
+
 func Jump(nums []int) int {
-	return 0
+	var dp []int
+
+	// A list of one number takes 0 steps to reach the end
+	if len(nums) < 2 {
+		return 0
+	}
+
+	minNonZero := func(ints ...int) int {
+		m := math.MaxInt
+		for _, i := range ints {
+			if i < m {
+				m = i
+			}
+		}
+		return m
+	}
+
+	dp = make([]int, len(nums))
+	for i := len(nums) - 2; i >= 0; i-- {
+		if nums[i] == 0 {
+			dp[i] = 0
+		} else {
+			rang := dp[i:min(len(dp), i+nums[i])]
+			dp[i] = dp[i] + 1 + minNonZero(rang...)
+		}
+	}
+
+	return dp[0]
 }

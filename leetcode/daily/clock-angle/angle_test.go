eaa4c3ac1cd37b136741f08b3b85a82b7e2d1651
aaronc <aaron.cassidy1995@gmail.com>
2026-06-18 21:02:10 +0100

    Add test cases

--- leetcode/daily/clock-angle/angle_test.go
+++ leetcode/daily/clock-angle/angle_test.go
@@ -10,7 +10,23 @@ func TestClockAngle(t *testing.T) {
 		hour     int
 		minute   int
 		expected float64
-	}{}
+	}{
+		"12:30": {
+			hour:     12,
+			minute:   30,
+			expected: 165,
+		},
+		"3:30": {
+			hour:     3,
+			minute:   30,
+			expected: 75,
+		},
+		"3:15": {
+			hour:     3,
+			minute:   15,
+			expected: 7.5,
+		},
+	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
